3cfaee6eed3a8de166a9f2f56c84b1afd0f61089
Mridhul <mridhul67@gmail.com>
2024-10-31 13:56:44 +0530

    Test: Add test for vertical ship placement

--- src/components/gameboard/gameBoard.test.js
+++ src/components/gameboard/gameBoard.test.js
@@ -9,3 +9,13 @@ test("place carrier horizontal", () => {
   expect(playerBoard.getBoard().get("0,3")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("0,4")).toEqual("Carrier");
 });
+
+test("place carrier vertical", () => {
+  const playerBoard = gameBoardFactory();
+  playerBoard.placeShip("Carrier", "0", "0", "vertical");
+  expect(playerBoard.getBoard().get("0,0")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("1,0")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("2,0")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("3,0")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("4,0")).toEqual("Carrier");
+});
