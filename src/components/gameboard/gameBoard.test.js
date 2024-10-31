ba1ea9580f3c854d7a67a8d9db81736fd5b991c5
Mridhul <mridhul67@gmail.com>
2024-10-31 16:29:46 +0530

    Test: Write test for patrol boat placement

--- src/components/gameboard/gameBoard.test.js
+++ src/components/gameboard/gameBoard.test.js
@@ -19,3 +19,17 @@ test("place carrier vertical", () => {
   expect(playerBoard.getBoard().get("3,0")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("4,0")).toEqual("Carrier");
 });
+
+test("place patrol boat horizontal", () => {
+  const playerBoard = gameBoardFactory();
+  playerBoard.placeShip("Patrol Boat", "0", "0", "horizontal");
+  expect(playerBoard.getBoard().get("0,0")).toEqual("Patrol Boat");
+  expect(playerBoard.getBoard().get("0,1")).toEqual("Patrol Boat");
+});
+
+test("place patrol boat vertical", () => {
+  const playerBoard = gameBoardFactory();
+  playerBoard.placeShip("Patrol Boat", "0", "0", "vertical");
+  expect(playerBoard.getBoard().get("0,0")).toEqual("Patrol Boat");
+  expect(playerBoard.getBoard().get("1,0")).toEqual("Patrol Boat");
+});
