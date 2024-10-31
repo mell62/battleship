9d3fe43350f6f715e74b7966aff83cdb2d2d47a0
Mridhul <mridhul67@gmail.com>
2024-10-31 16:50:24 +0530

    Test: Correct expected coordinates for given direction

--- src/components/gameboard/gameBoard.test.js
+++ src/components/gameboard/gameBoard.test.js
@@ -4,44 +4,44 @@ test("place carrier horizontal", () => {
   const playerBoard = gameBoardFactory();
   playerBoard.placeShip("Carrier", "0", "0", "horizontal");
   expect(playerBoard.getBoard().get("0,0")).toEqual("Carrier");
-  expect(playerBoard.getBoard().get("0,1")).toEqual("Carrier");
-  expect(playerBoard.getBoard().get("0,2")).toEqual("Carrier");
-  expect(playerBoard.getBoard().get("0,3")).toEqual("Carrier");
-  expect(playerBoard.getBoard().get("0,4")).toEqual("Carrier");
-});
-
-test("place carrier vertical", () => {
-  const playerBoard = gameBoardFactory();
-  playerBoard.placeShip("Carrier", "0", "0", "vertical");
-  expect(playerBoard.getBoard().get("0,0")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("1,0")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("2,0")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("3,0")).toEqual("Carrier");
   expect(playerBoard.getBoard().get("4,0")).toEqual("Carrier");
 });
 
+test("place carrier vertical", () => {
+  const playerBoard = gameBoardFactory();
+  playerBoard.placeShip("Carrier", "0", "0", "vertical");
+  expect(playerBoard.getBoard().get("0,0")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("0,1")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("0,2")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("0,3")).toEqual("Carrier");
+  expect(playerBoard.getBoard().get("0,4")).toEqual("Carrier");
+});
+
 test("place patrol boat horizontal", () => {
   const playerBoard = gameBoardFactory();
   playerBoard.placeShip("Patrol Boat", "0", "0", "horizontal");
   expect(playerBoard.getBoard().get("0,0")).toEqual("Patrol Boat");
-  expect(playerBoard.getBoard().get("0,1")).toEqual("Patrol Boat");
+  expect(playerBoard.getBoard().get("1,0")).toEqual("Patrol Boat");
 });
 
 test("place patrol boat vertical", () => {
   const playerBoard = gameBoardFactory();
   playerBoard.placeShip("Patrol Boat", "0", "0", "vertical");
   expect(playerBoard.getBoard().get("0,0")).toEqual("Patrol Boat");
-  expect(playerBoard.getBoard().get("1,0")).toEqual("Patrol Boat");
+  expect(playerBoard.getBoard().get("0,1")).toEqual("Patrol Boat");
 });
 
 test("check non-carrier cell horizontal", () => {
   const playerBoard = gameBoardFactory();
   playerBoard.placeShip("Carrier", "0", "0", "horizontal");
-  expect(playerBoard.getBoard().get("0,5")).toEqual(undefined);
+  expect(playerBoard.getBoard().get("5,0")).toEqual(undefined);
 });
 
 test("check non-patrol boat cell horizontal", () => {
   const playerBoard = gameBoardFactory();
   playerBoard.placeShip("Patrol Boat", "0", "0", "horizontal");
-  expect(playerBoard.getBoard().get("0,3")).toEqual(undefined);
+  expect(playerBoard.getBoard().get("3,0")).toEqual(undefined);
 });
