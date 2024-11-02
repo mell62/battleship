2e278beb830736124899643412048c2fa5cd8684
Mridhul <mridhul67@gmail.com>
2024-11-02 19:43:40 +0530

    Feat: Create module for controlling game flow, create boards and place ships

---
+++ src/components/gameController.js
@@ -0,0 +1,16 @@
+import { playerFactory } from "./player";
+
+const player = playerFactory();
+const computer = playerFactory();
+
+player.board.placeShip("Carrier", "2", "3", "vertical");
+player.board.placeShip("Battleship", "9", "5", "vertical");
+player.board.placeShip("Destroyer", "7", "1", "horizontal");
+player.board.placeShip("Submarine", "4", "4", "horizontal");
+player.board.placeShip("Patrol Boat", "5", "7", "horizontal");
+
+computer.board.placeShip("Carrier", "1", "1", "horizontal");
+computer.board.placeShip("Battleship", "6", "3", "vertical");
+computer.board.placeShip("Destroyer", "0", "8", "horizontal");
+computer.board.placeShip("Submarine", "9", "0", "vertical");
+computer.board.placeShip("Patrol Boat", "7", "8", "horizontal");
