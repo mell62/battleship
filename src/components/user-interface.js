64c43f26a3aeca2d87fe940c7c59da9f2922e4a1
Mridhul <mridhul67@gmail.com>
2024-11-03 23:37:25 +0530

    Refactor: Construct code to find appropriate coord dom element into a function

--- src/components/user-interface.js
+++ src/components/user-interface.js
@@ -11,6 +11,13 @@ const computerBoardCoords = Array.from(
   computerBoardInterface.querySelectorAll("*"),
 );
 
+const findCoordEle = function findCoordEle(coordsArray, coordClass) {
+  const coordEle = coordsArray.find((coord) => {
+    return coord.classList.contains(coordClass);
+  });
+  return coordEle;
+};
+
 function renderPlayerBoard() {
   const gameBoard = getPlayerBoard();
   const shipCoords = gameBoard.getShipsCoords();
@@ -18,16 +25,12 @@ function renderPlayerBoard() {
   shipCoords.forEach((coord) => {
     if (coord.direction === "horizontal") {
       const coordClass = `${coord.xCoord}-${coord.yCoord}`;
-      const coordEle = playerBoardCoords.find((playerCoord) => {
-        return playerCoord.classList.contains(coordClass);
-      });
+      const coordEle = findCoordEle(playerBoardCoords, coordClass);
       coordEle.textContent = "H";
       return;
     }
     const coordClass = `${coord.xCoord}-${coord.yCoord}`;
-    const coordEle = playerBoardCoords.find((playerCoord) => {
-      return playerCoord.classList.contains(coordClass);
-    });
+    const coordEle = findCoordEle(playerBoardCoords, coordClass);
     coordEle.textContent = "V";
   });
 }
