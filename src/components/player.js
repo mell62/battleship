4dd551af43e6b740b3929ccae6b47effc2ee7bbc
Mridhul <mridhul67@gmail.com>
2024-11-05 00:43:17 +0530

    Feat: Implement logic for computer to not attack already hit cells

--- src/components/player.js
+++ src/components/player.js
@@ -10,5 +10,13 @@ function playerFactory() {
 const doComputerAttack = function doComputerAttack(gameBoard) {
   const xCoord = Math.floor(Math.random() * 10).toString();
   const yCoord = Math.floor(Math.random() * 10).toString();
+  if (
+    gameBoard.getBoard().get(`${xCoord},${yCoord}`) === "Hit" ||
+    gameBoard.getBoard().get(`${xCoord},${yCoord}`) === "Miss"
+  ) {
+    // To avoid already attacked cells to be attacked again
+    doComputerAttack(gameBoard);
+    return;
+  }
   gameBoard.receiveAttack(xCoord, yCoord);
 };
