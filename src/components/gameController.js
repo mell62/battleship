aa30c2c1975ac004059a2dbc11e60038d7f49eda
Mridhul <mridhul67@gmail.com>
2024-11-10 00:51:27 +0530

    Style: Remove console log

--- src/components/gameController.js
+++ src/components/gameController.js
@@ -38,6 +38,5 @@ computerBoard.addEventListener("click", (event) => {
     doAttack(player.board, computer.board, event.target);
     renderComputerBoard();
     renderPlayerBoard();
-    console.log(event.target);
   }
 });
